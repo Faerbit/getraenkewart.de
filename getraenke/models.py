7603a47221b69be36b74bd936b4c6a450c67ff2b
Faerbit <faerbit@server.fake>
2013-07-06 00:08:25 +0200

    Initial Commit

---
+++ getraenke/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Monat (models.Model):
+	bierstriche = models.IntegerField(default=0)
+	colasnacktriche = models.IntegerField(default=0)
+	#snackstriche = models.IntegerField(default=0)
+	bezahlt = models.IntegerField(default=0)
+
+	def add_monat():
+		m = Monat()
+		m.save()
+		return m
+
+class Jahr(models.Model):
+	#person = models.ForeignKey(Person)
+	sonstige_kosten = models.IntegerField(default=0)
+	bemerkung = models.TextField()
+	jahr = models.IntegerField()
+	januar = models.ForeignKey(Monat, related_name = "+")
+	februar = models.ForeignKey(Monat, related_name = "+")
+	maerz  = models.ForeignKey(Monat, related_name = "+")
+	april = models.ForeignKey(Monat, related_name = "+")
+	mai  = models.ForeignKey(Monat, related_name = "+")
+	juni  = models.ForeignKey(Monat, related_name = "+")
+	juli  = models.ForeignKey(Monat, related_name = "+")
+	august  = models.ForeignKey(Monat, related_name = "+")
+	september  = models.ForeignKey(Monat, related_name = "+")
+	oktober  = models.ForeignKey(Monat, related_name = "+")
+	november  = models.ForeignKey(Monat, related_name = "+")
+	dezember  = models.ForeignKey(Monat, related_name = "+")
+
+	def __str__(self):
+		return str(self.jahr)
+
+class Person(models.Model):
+	name = models.CharField(max_length=50)
+	user = models.ForeignKey (User,default=-1)
+	jahre = models.ManyToManyField(Jahr)
+
+	def __str__(self):
+		return self.name
+	def add_year(self, jahr):
+		jan, feb, mae, apr, mai, jun, jul, aug, sep, okt, nov, dez = Monat(), Monat(), Monat(), Monat(), Monat(), Monat(), Monat(), Monat(), Monat(), Monat(), Monat(), Monat()
+		for i in [jan, feb, mae, apr, mai, jun, jul, aug, sep, okt, nov, dez]:
+			i.save()
+		neues_jahr = Jahr(jahr = jahr, januar = jan, februar = feb, maerz = mae, april = apr, mai = mai, juni = jun, juli = jul, august = aug, september = sep, oktober= okt, november = nov, dezember = dez)
+		neues_jahr.save()
+		self.jahre.add(neues_jahr)
+		self.save()
---
+++ getraenke/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from getraenke.models import Person, Jahr, Monat
+from datetime import date
+
+def generate_bier_chart (year):
+	#xdata = ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"]
+	"""xdata = []
+	for i in range(1,13):
+		xdata.append(i)"""
+	xdata = []
+	for i in range(1, 13):
+		xdata.append(date(year, i, 1))
+	chartdata = { 'x' : xdata}
+	count = 0
+	for i in Person.objects.all():
+		count += 1
+		for j in i.jahre.all():
+			if (j.jahr == year):
+				ydata = [j.januar.bierstriche, j.februar.bierstriche, j.maerz.bierstriche,
+					j.april.bierstriche, j.mai.bierstriche, j.juni.bierstriche,j.juli.bierstriche, j.august.bierstriche, 
+					j.september.bierstriche, j.oktober.bierstriche, j.november.bierstriche, j.dezember.bierstriche]
+				chartdata['name' + str(count)] = str(i)
+				chartdata['y' + str(count)] = ydata
+				#extra_serie = {"tooltip": {"y_start": "", "y_end": " cal"}}
+				#chartdata['extra' + str(count)] = extra_serie
+	charttype = "lineChart"
+	data = {
+			'charttype': charttype,
+			'charttdata': chartdata
+			}
+	return data
