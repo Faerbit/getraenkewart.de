b2ab0705b9cf475c22ea145fd586f9a26ef02230
Faerbit <faerbit@server.fake>
2013-10-09 23:33:39 +0200

    Implemented login. Currently not working.

--- getraenkewart/urls.py
+++ getraenkewart/urls.py
@@ -18,4 +18,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^$', views.index, name='index'),
     url(r'^login/', views.login, name='login'),
+    url(r'^logout/', views.logout, name='logout'),
 )
--- getraenkewart/views.py
+++ getraenkewart/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
 from getraenke.views import generate_bier_chart
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
-	return render (request, "getraenkewart/index.html", generate_bier_chart(2013))
+    context = dict(list({'chart':generate_bier_chart(2013)}.items()) + list(standard_checks(request).items()))
+    return render (request, "getraenkewart/index.html", context)
 
 def login(request):
-    return render (request, "getraenkewart/index.html")
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate (username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(user)
+            context = standard_checks(request, "success", "Erfolgreich eingeloggt!")
+            return render (request, "getraenkewart/index.html", context)
+        else:
+            context = standard_checks(request, "danger", "Du bist noch nicht aktiviert! Frag mal nach.")
+            return render (request, "getraenkewart/index.html", context)
+    else:
+        context = standard_checks(request, "danger", "Dein Passwort oder dein Benutzername ist falsch.")
+        return render (request, "getraenkewart/index.html", context)
+
+def logout(request):
+    logout(request)
+    context = standard_checks(request, "success", "Erfolgreich ausgeloggt!")
+    return render(request, "getraenkewart/index.html", context)
+
+def standard_checks(request, alert_type="", alert_message=""):
+    if request.user.is_authenticated():
+        name = request.user.first_name
+    else:
+        name = ""
+    if alert_type == "":
+        return {'name':name, 'alert_type':"", 'alert_message':""}
+    else:
+        return {'name':name, 'alert_type':alert_type, 'alert_message':alert_message}
