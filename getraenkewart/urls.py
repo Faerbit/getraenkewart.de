e424555bd16b221e5adfc1a91cc52b3f9e3a6c64
Faerbit <faerbit@server.fake>
2013-10-10 17:20:38 +0200

    Implemented registration

--- getraenkewart/urls.py
+++ getraenkewart/urls.py
@@ -19,4 +19,5 @@ urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
     url(r'^login/', views.login_view, name='login'),
     url(r'^logout/', views.logout_view, name='logout'),
+    url(r'^register/', views.register, name='register'),
 )
--- getraenkewart/views.py
+++ getraenkewart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from getraenke.views import generate_bier_chart
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def index(request):
     context = dict(list({'chart':generate_bier_chart(2013)}.items()) + list(standard_checks(request).items()))
@@ -26,6 +27,40 @@ def logout_view(request):
     logout(request)
     context = standard_checks(request, "success", "Erfolgreich ausgeloggt!")
     return render(request, "getraenkewart/index.html", context)
+
+def register(request):
+    if request.method =="GET":
+        context = standard_checks(request)
+        return render(request, "getraenkewart/register.html", context)
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
+        data = {'first_name':first_name, 'last_name':last_name, 'username':username, 'password':password}
+        for i in User.objects.all():
+            if i.username==username:
+                context = dict({'username_error':True})
+                context.update(standard_checks(request, "danger", "Dieser Benutzername ist bereits vergeben!"))
+                context.update(data)
+                return render(request, "getraenkewart/register.html", context)
+        if first_name=="":
+            context = dict({'first_name_error':True})
+            context.update(standard_checks(request, "danger", "Bitte gib einen Vornamen ein."))
+            context.update(data)
+            return render(request, "getraenkewart/register.html", context)
+        if len(password) < 8:
+            context = dict({'password_error':True})
+            context.update(standard_checks(request, "danger", "Das Passwort muss mindestens 8 Zeichen lang sein."))
+            context.update(data)
+            return render(request, "getraenkewart/register.html", context)
+        user = User.objects.create_user(username, "", password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.is_active = False
+        user.save()
+        context = standard_checks(request, "success", "Neuen Benutzer erfolgreich angelegt. Bitte warte auf deine Aktivierung.")
+        return render(request, "getraenkewart/index.html", context)
 
 def standard_checks(request, alert_type="", alert_message=""):
     if request.user.is_authenticated():
