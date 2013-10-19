027f8f144c8daf15910eb6b75d5974dcd23e222f
Faerbit <faerbit@server.fake>
2013-10-19 15:48:56 +0200

    Changed alerts to use the django message framework

--- getraenkewart/views.py
+++ getraenkewart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 #from getraenke.views import generate_bier_chart
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 def index(request):
     #context = dict(list({'chart':generate_bier_chart(2013)}.items()) + list(standard_checks(request).items()))
@@ -15,18 +16,22 @@ def login_view(request):
     if user is not None:
         if user.is_active:
             login(request, user)
-            context = standard_checks(request, "start",  "success", "Erfolgreich eingeloggt!")
+            messages.success(request, "Erfolgreich eingeloggt!")
+            context = standard_checks(request, "start")
             return render (request, "getraenkewart/index.html", context)
         else:
-            context = standard_checks(request, "start", "danger", "Du bist noch nicht aktiviert! Frag mal nach.")
+            messages.error(request, "Du bist noch nicht aktiviert! Frag mal nach.")
+            context = standard_checks(request, "start")
             return render (request, "getraenkewart/index.html", context)
     else:
-        context = standard_checks(request, "start", "danger", "Dein Passwort oder dein Benutzername ist falsch.")
+        messages.error(request, "Dein Passwort oder dein Benutzername ist falsch.")
+        context = standard_checks(request, "start")
         return render (request, "getraenkewart/index.html", context)
 
 def logout_view(request):
     logout(request)
-    context = standard_checks(request, "start", "success", "Erfolgreich ausgeloggt!")
+    message.success(request, "Erfolgreich ausgeloggt!")
+    context = standard_checks(request, "start")
     return render(request, "getraenkewart/index.html", context)
 
 def register(request):
@@ -39,36 +44,53 @@ def register(request):
         username = request.POST['username']
         password = request.POST['password']
         data = {'first_name':first_name, 'last_name':last_name, 'username':username, 'password':password}
-        for i in User.objects.all():
-            if i.username==username:
-                context = dict({'username_error':True})
-                context.update(standard_checks(request, "start", "danger", "Dieser Benutzername ist bereits vergeben!"))
-                context.update(data)
-                return render(request, "getraenkewart/register.html", context)
+        error = False
+        context = dict()
+        if User.objects.filter(username__exact=username).exists():
+            error = True
+            context.update(dict({'username_error':True}))
+            messages.error(request, "Dieser Benutzername ist bereits vergeben!")
         if first_name=="":
-            context = dict({'first_name_error':True})
-            context.update(standard_checks(request, "start", "danger", "Bitte gib einen Vornamen ein."))
-            context.update(data)
-            return render(request, "getraenkewart/register.html", context)
+            error = True
+            context.update(dict({'first_name_error':True}))
+            messages.error(request, "Bitte gib einen Vornamen ein.")
+        if last_name=="":
+            error = True
+            context.update(dict({'last_name_error':True}))
+            messages.error(request, "Bitte gib einen Nachnamen ein.")
         if len(password) < 8:
-            context = dict({'password_error':True})
-            context.update(standard_checks(request, "start", "danger", "Das Passwort muss mindestens 8 Zeichen lang sein."))
+            error = True
+            context.update(dict({'password_error':True}))
+            messages.error(request, "Das Passwort muss mindestens 8 Zeichen lang sein.")
+        if len(username) > 30:
+            error = True
+            context.update(dict({'username_error':True}))
+            messages.error(request, "Der Benutzername darf nicht länger als 30 Zeichen sein.")
+        if len(first_name) > 30:
+            error = True
+            context.update(dict({'first_name_error':True}))
+            messages.error(request, "Der Vorname darf nicht länger als 30 Zeichen sein.")
+        if len(last_name) > 30:
+            error = True
+            context.update(dict({'last_name_error':True}))
+            messages.error(request, "Der Nachname darf nicht länger als 30 Zeichen sein.")
+        if not error:
+            user = User.objects.create_user(username, "", password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.is_active = False
+            user.save()
+            messages.success(request, "Neuen Benutzer erfolgreich angelegt. Bitte warte auf deine Aktivierung.")
+            context = standard_checks(request, "start")
+            return render(request, "getraenkewart/index.html", context)
+        else:
+            context.update(standard_checks(request, "start"))
             context.update(data)
             return render(request, "getraenkewart/register.html", context)
-        user = User.objects.create_user(username, "", password)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.is_active = False
-        user.save()
-        context = standard_checks(request, "start", "success", "Neuen Benutzer erfolgreich angelegt. Bitte warte auf deine Aktivierung.")
-        return render(request, "getraenkewart/index.html", context)
 
-def standard_checks(request, active_nav, alert_type="", alert_message=""):
+def standard_checks(request, active_nav):
     if request.user.is_authenticated():
         name = request.user.first_name
     else:
         name = ""
-    if alert_type == "":
-        return {'name':name, 'active_nav':active_nav, 'alert_type':"", 'alert_message':""}
-    else:
-        return {'name':name, 'active_nav':active_nav, 'alert_type':alert_type, 'alert_message':alert_message}
+    return {'name':name, 'active_nav':active_nav}
