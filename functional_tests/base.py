faa7d21d6414ae83447eefab3ac5d86e271fb38c
Faerbit <faerbit@gmail.com>
2014-03-01 21:02:29 +0100

    Added LoggedInTest class.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from django.contrib.auth.models import User
 import sys
 
 class FunctionalTest(LiveServerTestCase):
@@ -9,13 +10,15 @@ class FunctionalTest(LiveServerTestCase):
         for arg in sys.argv:
             if "liveserver" in arg:
                 cls.server_url = "http://" + arg.split("=")[1]
+                cls.against_staging = True
                 return
         LiveServerTestCase.setUpClass()
+        cls.against_staging = False
         cls.server_url = cls.live_server_url
 
     @classmethod
     def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
+        if not cls.against_staging:
             LiveServerTestCase.tearDownClass()
     
     def setUp(self):
@@ -24,3 +27,22 @@ class FunctionalTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+
+class LoggedInTest(FunctionalTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        if not cls.against_staging:
+            User.objects.create_user("chris", "chris@provider.com", "secret12")
+        else:
+            self.fail("Implement against staging!")
+
+    def setUp(self):
+        super().setUp()
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id("login-dropdown-button").click()
+        self.browser.find_element_by_id("username").send_keys("chris")
+        self.browser.find_element_by_id("password").send_keys("secret12")
+        self.browser.find_element_by_id("login-button").click()
